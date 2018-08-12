ac5c90546007207bab2dde67d832513187bfc7a1
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-08-12 12:51:00 -0400

    Day 3 complete

---
+++ CharCounter/30DaysTDD.UnitTests/StringUtilsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using CharCounterLibrary;
+
+namespace _30DaysTDD.UnitTests
+{
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        [Test]
+        public void ShouldBeAbleToCountCharInSimpleSentence()
+        {
+            //arrange
+            var input = "TDD is awesome!";
+            var charToScanFor = "e";
+            var expectedResult = 2;
+
+            //Act
+            var stringUtils = new CharCounter();
+            int result = CharCounter.CountChar(input, charToScanFor);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
---
+++ CharCounter/CharCounterLibrary/CharCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace CharCounterLibrary
+{ 
+    public class CharCounter
+    {
+        public static int CountChar(string input, string scanTarget)
+        {
+            return 0;
+        }
+
+    }
+    
+}
