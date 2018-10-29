c158cc7b5f32b94dc5bb04ad87cd91369cbc0160
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-10-29 19:08:02 -0400

    Finished Day 3

---
+++ ThirtyDaysTDD/ThirtyDaysTDD/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ThirtyDaysTDD
+{
+    public class Class1
+    {
+    }
+}
---
+++ ThirtyDaysTDD/ThirtydaysTDD.UnitTests/StringUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ThirtydaysTDD.UnitTests
+{
+    [TestFixture]
+    public class StringUtils
+    {
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            //Arrange
+            var sentenceToScan = "TDD is awesome!";
+            var characterToScanFor = "e";
+            var expectedResult = 2;
+            var stringUtils = new StringUtils();
+
+            //Act
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            //Assert
+        }
+    }
+}
